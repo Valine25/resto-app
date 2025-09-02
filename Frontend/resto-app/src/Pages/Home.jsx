dbc256d27530de8cd393c914bba2a95b2b6b3044
Valine <noronhavaline@gmail.com>
2025-09-02 23:01:17 +0530

    Added Qualities section

--- Frontend/resto-app/src/Pages/Home.jsx
+++ Frontend/resto-app/src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../components/About";
 import HeroSection from "../components/Herosection";
+import Qualities from "../components/Qualities";
 
 export default function Home() {
   return (
     <>
       <HeroSection/>
       <About/>
+      <Qualities/>
     </>
   );
 }
